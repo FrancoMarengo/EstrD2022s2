e44caed45ad0d2cdcf6ae4fb3ee39b042f1456d2
Franco Marengo <francomarengo.21@gmail.com>
2022-09-08 13:30:21 -0300

    Agrego ejercicios de camino al tesoro. A terminar.

--- tp-3/tp-3.hs
+++ tp-3/tp-3.hs
@@ -38,3 +38,73 @@ ponerN :: Int -> Color -> Celda -> Celda
 ponerN 0 c cel = cel
 ponerN n c cel = (Bolita c (ponerN (n-1) c cel))
 
+-- 1.2
+data Objeto = Cacharro | Tesoro
+            deriving Show
+data Camino = Fin | Cofre [Objeto] Camino | Nada Camino
+            deriving Show
+
+-- Indica si hay un cofre con un tesoro en el camino.
+hayTesoro :: Camino -> Bool
+hayTesoro Fin           = False
+hayTesoro (Nada c)      = False || hayTesoro c
+hayTesoro (Cofre obs c) = hayTesoro' obs || hayTesoro c
+
+-- (Funcion auxiliar) Dada una lista de objetos indica si hay al menos un tesoro.
+hayTesoro' :: [Objeto] -> Bool
+hayTesoro' []       = False
+hayTesoro' (ob:obs) = esTesoro ob || hayTesoro' obs
+
+-- (Funcion auxiliar) Dado un objeto indica si es tesoro
+esTesoro :: Objeto -> Bool
+esTesoro Tesoro = True
+esTesoro _      = False
+
+-- Indica la cantidad de pasos que hay que recorrer hasta llegar al primer cofre con un tesoro.
+-- Si un cofre con un tesoro está al principio del camino, la cantidad de pasos a recorrer es 0.
+-- Precondición: tiene que haber al menos un tesoro.
+pasosHastaTesoro :: Camino -> Int
+pasosHastaTesoro Fin           = error "No hay ningun tesoro en este camino."
+pasosHastaTesoro (Nada c)      = 1 + pasosHastaTesoro c
+pasosHastaTesoro (Cofre obs c) = if hayTesoro' obs 
+                                  then 0
+                                  else 1 + pasosHastaTesoro c
+
+-- Indica si hay un tesoro en una cierta cantidad exacta de pasos. Por ejemplo, si el número de
+-- pasos es 5, indica si hay un tesoro en 5 pasos.
+hayTesoroEn :: Int -> Camino -> Bool
+hayTesoroEn 0 _             = False 
+hayTesoroEn n (Fin)         = False 
+hayTesoroEn n (Nada c)      = hayTesoroEn (n-1) c
+hayTesoroEn n (Cofre obs c) = hayTesoro' obs || hayTesoroEn (n-1) c
+
+-- Indica si hay al menos “n” tesoros en el camino.
+alMenosNTesoros :: Int -> Camino -> Bool
+alMenosNTesoros 0 _             = True
+alMenosNTesoros n (Fin)         = False
+alMenosNTesoros n (Nada c)      = alMenosNTesoros n c
+alMenosNTesoros n (Cofre obs c) = if hayTesoro' obs
+                                   then alMenosNTesoros (tesorosFaltantesParaN n obs) c
+                                   else alMenosNTesoros n c
+
+-- (Funcion auxiliar) Retorna la cantidad de tesoros que faltan para llegar a "n" cantidad de tesoros, 
+-- restando los tesoros de la lista de objetos dada.
+tesorosFaltantesParaN :: Int -> [Objeto] -> Int
+tesorosFaltantesParaN 0 _        = 0
+tesorosFaltantesParaN n []       = n
+tesorosFaltantesParaN n (ob:obs) = if esTesoro ob 
+                                    then tesorosFaltantesParaN (n-1) obs
+                                    else tesorosFaltantesParaN n obs 
+
+-- Dado un rango de pasos, indica la cantidad de tesoros que hay en ese rango. Por ejemplo, si el rango es 3 y 5, 
+-- indica la cantidad de tesoros que hay entre hacer 3 pasos y hacer 5. Están incluidos tanto 3 como 5 en el resultado.
+cantTesorosEntre :: Int -> Int -> Camino -> Int -- Sin terminar
+cantTesorosEntre 0 n2 c (Fin)         = 0
+cantTesorosEntre 0 n2 c (Nada c)      = 0
+cantTesorosEntre 0 n2 c (Cofre obs c) = 0
+cantTesorosEntre n1 0 c (Fin)         = 0
+cantTesorosEntre n1 0 c (Nada c)      = 0
+cantTesorosEntre n1 0 c (Cofre obs c) = 0
+cantTesorosEntre n1 n2 (Fin)          = cantTesorosEntre (n1-1) (n2-1) c
+cantTesorosEntre n1 n2 (Nada c)       = cantTesorosEntre (n1-1) (n2-1) c
+cantTesorosEntre n1 n2 (Cofre obs c)  = cantTesorosEntre (n1-1) (n2-1) c
