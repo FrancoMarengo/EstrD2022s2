70a24f7952cd3710eaf4a1afdffaf087cbe6a1b5
Marengo, Franco Martín <87328857+FrancoMarengo@users.noreply.github.com>
2022-09-29 17:54:32 -0300

    Agrego ejercicios de usuario de Map.
    No funciona ya que no se importa ni está realizado el modulo de map.

--- tp-6/tp-6.hs
+++ tp-6/tp-6.hs
@@ -49,24 +49,60 @@ listToMap :: Eq k => [(k, v)] -> Map k v
 listToMap []          = emptyM
 listToMap ((k,v):kvs) = assocM k v (listToMap kvs)
 
--------------------------------------------
--------------------------------------------
-
+-- O(n^2) ya que utiliza tuplasParaKeys.
 -- Propósito: convierte un map en una lista de pares clave valor.
 mapToList :: Eq k => Map k v -> [(k, v)]
 mapToList m = tuplasParaKeys (keys m) m
 
+-- O(n^2) ya que por cada key realiza la operación lookupM (O(n) en el peor caso).
+-- (Funcion auxiliar) Propósito: dada una lista de claves y un map, retorna una lista de pares 
+--                               compuestos por cada llave dada con su valor asociado.
+-- Precond: todas las keys dadas deben tener un valor asociado en el map dado.
 tuplasParaKeys :: Eq k => [k] -> Map k v -> [(k, v)]
 tuplasParaKeys []     _ = []
-tuplasParaKeys (k:ks) m = (k, lookupM k m) : tuplasParaKeys ks m
+tuplasParaKeys (k:ks) m = (k, fromJust (lookupM k m)) : tuplasParaKeys ks m
 
+-- O(n^2) ya que por cada par de la lista dada, realiza agregarClaveValor, que en el peor caso es lineal.
 -- Propósito: dada una lista de pares clave valor, agrupa los valores de los pares que compartan la misma clave.
 agruparEq :: Eq k => [(k, v)] -> Map k [v]
+agruparEq []       = emptyM
+agruparEq (kv:kvs) = agregarClaveValor kv (agruparEq kvs)
 
+-- O(n*M) siendo n la cantidad de elementos en el map y M el costo operacional de assocM/lookupM
+--        que en el peor caso alguna de las dos es lineal.
+-- (Funcion auxiliar) Propósito: dado un par clave-valor y un map clave-valores, si la clave se encuentra en el 
+--                               map asocia la clave, con los valores previos pero agregandole la clave dada.
+--                               Si la clave no se encuentra en el map, asocia la clave con una lista singular
+--                               que contiene el valor dado.
+agregarClaveValor :: Eq => (k, v) -> Map k [v] -> Map k [v]
+agregarClaveValor (k,v) m = 
+    if isNothing (lookupM k m)
+     then assocM k [v] m
+     else assocM k (v : (fromJust (lookupM k m)) m
+
+-- O(n^2) ya que por cada clave dada, realiza lookupM/assocM que en el peor caso alguna es lineal.
 -- Propósito: dada una lista de claves de tipo k y un map que va de k a Int, le suma uno a
 -- cada número asociado con dichas claves.
 incrementar :: Eq k => [k] -> Map k Int -> Map k Int
+incrementar [] m     = m
+incrementar (k:ks) m = 
+    if isNothing (lookupM k m)
+     then incrementar ks m
+     else assocM k (1 + fromJust (lookupM k m)) (incrementar ks m)
 
+-- O(N*M) siendo N el tamaño del primer map dado y M el costo operacional de agregarParesKV, que en 
+--        el peor caso es cuadrática.
 -- Propósito: dado dos maps se agregan las claves y valores del primer map en el segundo. Si
 -- una clave del primero existe en el segundo, es reemplazada por la del primero.
 mergeMaps:: Eq k => Map k v -> Map k v -> Map k v
+mergeMaps m1 m2 = agregarParesKV (mapToList m1) m2
+
+-- O(N*M) siendo N la longitud de la lista dada y M el costo operacional de assocM, que en el peor caso es lineal.
+--        (en ese caso la funcion es n^2)
+-- (Funcion auxiliar) Propósito: dada una lista de pares clave-valor y un map clave-valor,
+--                               retorna el map con las claves dadas, asociadas a los valores dados.
+--                               Si una clave ya existia en el map, se sobreescribe con el valor 
+--                               dado.
+agregarParesKV :: Eq k => [(k, v)] -> Map k v -> Map k v
+agregarParesKV [] m          = m 
+agregarParesKV ((k,v):kvs) m = assocM k v (agregarParesKV kvs m) 
