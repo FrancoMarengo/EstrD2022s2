f94c745d186364c0b8649893ae550dae6f4b02a3
Franco Marengo <francomarengo.21@gmail.com>
2022-10-08 15:48:57 -0300

    agrego ejercicio 4 de p7.

--- tp-7/Empresa.hs
+++ tp-7/Empresa.hs
@@ -13,8 +13,11 @@ module Empresa (
 
 where
 
+-- imports necesarios para funcionar:
+
 -- import MapBST
 -- import SetBST
+-- import Empleado
 
 type SectorId = Int
 type CUIL = Int
@@ -64,13 +67,77 @@ agregarSector id (ConsE ms mc) = ConsE (assocM id emptyS ms) mc
 
 -- Propósito: agrega un empleado a la empresa, en el que trabajará en dichos sectores y tendrá
 -- el CUIL dado.
--- Costo: calcular.
+-- Precond: los sectorId dados deben estar en la empresa dada
+-- Costo: O(s * M * log S * log E) siendo s la cantidad de sectores dados, M el costo operacional de
+--                                 incorporarSector (logaritmico), S la cantidad de sectores
+--                                 de la empresa y E la cantidad de empleados de la empresa.
 agregarEmpleado :: [SectorId] -> CUIL -> Empresa -> Empresa
+agregarEmpleado ss c (ConsE ms mc) = 
+    let empleado = incorporarSectores ss (consEmpleado c) 
+    in ConsE (agregarASectores ss empleado ms) (registrar empleado mc)
+
+-- (Funcion auxiliar) Propósito: registra un empleado dado en el map de cuil-Empleado
+-- Costo: O(log C) siendo C la cantidad de cuil's en mc, debido a que usa assocM
+registrar :: Empleado -> Map CUIL Empleado -> Map CUIL Empleado
+registrar e mc = assocM (cuil e) e mc
+
+-- (Funcion auxiliar) Propósito: dado una lista de sectores y un empleado, incluye los sectores
+--                               a la estructura interna de empleado.
+-- Costo: O(s * log S) siendo s la cantidad de sectores dados y S la cantidad de sectores que el 
+--                     empleado tiene asignados.
+incorporarSectores :: [SectorId] -> Empleado -> Empleado
+incorporarSectores []     e = e 
+incorporarSectores (s:ss) e = incorporarSector s (incorporarSectores ss e)
+
+-- (Funcion auxiliar) Propósito: agrega un empleado a los sectores dados, en un map de sector-empleados
+-- Costo: O(s * (log S * log E)) siendo s la cantidad de sectores dados y (log S * log E) el costo de
+--                               la funcion incorporarASector 
+agregarASectores :: [SectorId] -> Empleado -> Map SectorId (Set Empleado) -> Map SectorId (Set Empleado)
+agregarASectores []     _ ms = ms 
+agregarASectores (s:ss) e ms = incorporarASector s e (agregarASectores ss e ms)
+
+-- (Funcion auxiliar) Propósito: agrega un empleado al sector dado, en un map de sector-empleados
+--                    Precond: el sectorId dado se encuentra en el map dado
+-- Costo: O(log S * log E) siendo S la cantidad de sectores en el map y E la cantidad de empleados en el set
+incorporarASector :: SectorId -> Empleado -> Map SectorId (Set Empleado) -> Map SectorId (Set Empleado)
+incorporarASector s e ms = assocM s (addS e (fromJust (lookupM s ms))) ms
 
 -- Propósito: agrega un sector al empleado con dicho CUIL.
--- Costo: calcular.
+-- Precond: el sectorId dado se encuentra en la empresa dada
+-- Costo: O(M * log S * log E) siendo M el costo operacional e incorporarSector (logaritmico)
+--                             S la cantidad de sectores de la empresa y E la cantidad de empleados
+--                             de la empresa
 agregarASector :: SectorId -> CUIL -> Empresa -> Empresa
+agregarASector s c (ConsE ms mc) = 
+    let empleado = incorporarSector s (fromJust (lookupM c mc)) 
+    in ConsE (incorporarASector s empleado ms) (assocM c empleado mc)
 
 -- Propósito: elimina al empleado que posee dicho CUIL.
--- Costo: calcular.
+-- Precond: el cuil del empleado debe estar registrado en la empresa
+-- Costo: O(log C * s * (log E * log S)) siendo C la cantidad de cuils en mc, s la cantidad de sectores
+--                                       asignados en el empleado, E la cantidad de empleados en el set
+--                                       y S la cantidad de sectores en el map ms.
 borrarEmpleado :: CUIL -> Empresa -> Empresa
+borrarEmpleado c (ConsE ms mc) = 
+    let empleado = fromJust (lookupM c mc)
+    in ConsE (borrarDeSectores (sectores empleado) empleado ms) (borrarDeCuils (cuil empleado) mc)
+
+-- (Funcion auxiliar) Propósito: borra a un empleado de un map cuil-empleado, segun un cuil dado
+--                    Precond: el cuil dado se encuentra registrado en el map
+-- Costo: O(log C) siendo C la cantidad de cuils registrados en el map
+borrarDeCuils :: CUIL -> Map CUIL Empleado -> Map CUIL Empleado
+borrarDeCuils c mc = deleteM c mc
+
+-- (Funcion auxiliar) Propósito: borra a un empleado de una lista de sectores dados en un map de sector-empleados
+--                    Precond: los sectores dados se encuentran en el map dado
+-- Costo: O(s * (log E * log S)) siendo s la longitud de la lista de sectores dados, E la cantidad de
+--                               empleados en el set y S la cantidad de sectores en el map
+borrarDeSectores :: [SectorId] -> Empleado -> Map SectorId (Set Empleado) -> Map SectorId (Set Empleado)
+borrarDeSectores []     _ ms = ms
+borrarDeSectores (s:ss) e ms = borrarDeSector s e (borrarDeSectores ss e ms)
+
+-- (Funcion auxiliar) Propósito: borra a un empleado de un sector dado en un map de sector-empleados
+--                    Precond: el sector dado se encuentra en el map dado
+-- Costo: O(log E * log S) siendo E la cantidad de empleados en el set y S la cantidad de sectores en el map
+borrarDeSector :: SectorId -> Empleado -> Map SectorId (Set Empleado) -> Map SectorId (Set Empleado)
+borrarDeSector s e ms = assocM s (removeS e (fromJust (lookupM s ms))) ms 
